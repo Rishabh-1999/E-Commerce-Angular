923f4190ae8bd95c7b5247c86009e5db654b2fc8
Rishabh-1999 <rishabhanand33@gmail.com>
2020-02-20 22:48:26 +0530

    Second Commit
    - Firebase Added
    - Navbar Added
    - Components Added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRqg5OHoIeE-IsNgdgD_6R3BhFczzbTPE",
+    authDomain: "e-commerce-angular-c3d8f.firebaseapp.com",
+    databaseURL: "https://e-commerce-angular-c3d8f.firebaseio.com",
+    projectId: "e-commerce-angular-c3d8f",
+    storageBucket: "e-commerce-angular-c3d8f.appspot.com",
+    messagingSenderId: "988959874650",
+    appId: "1:988959874650:web:d9d3600107bb4332894263",
+    measurementId: "G-8E3M0F3RQJ"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRqg5OHoIeE-IsNgdgD_6R3BhFczzbTPE",
+    authDomain: "e-commerce-angular-c3d8f.firebaseapp.com",
+    databaseURL: "https://e-commerce-angular-c3d8f.firebaseio.com",
+    projectId: "e-commerce-angular-c3d8f",
+    storageBucket: "e-commerce-angular-c3d8f.appspot.com",
+    messagingSenderId: "988959874650",
+    appId: "1:988959874650:web:d9d3600107bb4332894263",
+    measurementId: "G-8E3M0F3RQJ"
+  }
 };
 
 /*
